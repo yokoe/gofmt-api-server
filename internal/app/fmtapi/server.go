59948ab274fd66fa66ce501218b3f67a37a31885
Sota Yokoe <sota@kreuz45.com>
2018-11-11 22:19:04 +0900

    feat: added /format api which returns request body.

--- internal/app/fmtapi/server.go
+++ internal/app/fmtapi/server.go
@@ -1,6 +1,7 @@
 package fmtapi
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 )
@@ -17,9 +18,39 @@ func NewServer() *Server {
 // Run starts listening
 func (s *Server) Run() error {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/format", formatHandler)
+
 	return http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "fmtapi server")
+}
+
+func formatHandler(w http.ResponseWriter, r *http.Request) {
+	result, err := processFormatRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, err)
+		return
+	}
+	fmt.Fprintf(w, result)
+}
+
+func processFormatRequest(r *http.Request) (string, error) {
+	if r.Method != "POST" {
+		return "", fmt.Errorf("unsupported method %s", r.Method)
+	}
+
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(r.Body); err != nil {
+		return "", err
+	}
+	body := buf.String()
+
+	if len(body) == 0 {
+		return "", fmt.Errorf("no body")
+	}
+
+	return body, nil
 }
